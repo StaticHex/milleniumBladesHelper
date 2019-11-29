52d2e4cb34e76603cb6b4027fd1864f88ae98ef2
StaticHex <indegon1@gmail.com>
2019-11-29 16:51:36 -0600

    Adding documentation (killing technical debt)

--- loader.py
+++ loader.py
@@ -2,6 +2,9 @@ from __future__ import print_function, division
 from json import loads
 from mbcard import MBType, MBSet, MBCard
 
+# Set Loader Class
+# --------------------------------------------------------------------------------
+# Stub class used to load in data from json file and parse into a dictionary
 class SetLoader:
     def __init__(self):
         # Read in data from json file and store in temporary object
@@ -9,16 +12,24 @@ class SetLoader:
         jsonData = f.read()
         f.close()
         jsonObj = loads(jsonData)
+
+        # Create a dictionary to throw our data into
         self.setDict = {}
+
+        # Parse entire json file
         for expansion in jsonObj:
             for cardType in expansion:
                 if cardType != "name":
                     for setName in expansion[cardType]:
+                        # Create a new card object and store the name of the
+                        # enum object in a temp variable
                         newCard = MBCard(setName)
                         newCard.setCardType(cardType)
                         newCard.setExpansionSet(expansion["name"])
-
                         enumSetName = newCard.getExpansionSet().name
+
+                        # Safely append to our dict i.e. if we haven't added
+                        # to this set before, create an empty array before adding
                         if enumSetName not in self.setDict:
                             self.setDict[enumSetName] = []
                         self.setDict[enumSetName].append(newCard)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from __future__ import print_function, division
 from loader import SetLoader
 from mbcard import MBSet, MBType
 
+# Create our loader
 loader = SetLoader()
